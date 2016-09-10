4c244f409fc97744cdf55c48bf45b957d8e609be
Marc Fernandez <marc.fernandezg@gmail.com>
2016-09-09 19:30:30 -0700

    Added authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910015839) do
+ActiveRecord::Schema.define(version: 20160910022544) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "api_key"
   end
 
 end
