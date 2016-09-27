3cbe988d8cc15761b4d0d743b3f355bbbcb63810
Marc Fernandez <marc.fernandezg@gmail.com>
2016-09-26 23:37:25 -0700

    Added url to station serializer.

--- app/serializers/shoutcast/api/station_serializer.rb
+++ app/serializers/shoutcast/api/station_serializer.rb
@@ -1,5 +1,9 @@
 module Shoutcast::Api
   class StationSerializer < ActiveModel::Serializer
-    attributes :id, :name, :genres, :logo
+    attributes :id, :name, :genres, :logo, :url
+
+    def url
+      "http://yp.shoutcast.com/sbin/tunein-station.pls?id=#{object.id}"
+    end
   end
 end
