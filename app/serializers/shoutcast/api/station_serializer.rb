32b54e32c342c6d28a69d0813f804c740136066d
Marc Fernandez <marc.fernandezg@gmail.com>
2016-12-19 22:23:48 -0800

    Changed genres for genre.

--- app/serializers/shoutcast/api/station_serializer.rb
+++ app/serializers/shoutcast/api/station_serializer.rb
@@ -1,6 +1,10 @@
 module Shoutcast::Api
   class StationSerializer < ActiveModel::Serializer
-    attributes :id, :name, :genres, :logo, :url
+    attributes :id, :name, :genre, :logo, :url
+
+    def genre
+      object.genres.first
+    end
 
     def url
       "http://yp.shoutcast.com/sbin/tunein-station.pls?id=#{object.id}"
