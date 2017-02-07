967891c1ef0c66c48f1796ff3a825a16d20b90c0
Marc Fernandez <marc.fernandezg@gmail.com>
2017-02-06 21:47:55 -0800

    Removed API subdomain.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  constraints subdomain: 'api' do
-    scope module: 'api' do
-      namespace :v1 do
-        resources :sessions, only: [:create]
-        resources :users, only: [:create, :update]
-        resources :stations, only: [:index] do
-          collection do
-            get 'search'
-          end
+  scope module: 'api' do
+    namespace :v1 do
+      resources :sessions, only: [:create]
+      resources :users, only: [:create, :update]
+      resources :stations, only: [:index] do
+        collection do
+          get 'search'
         end
       end
     end
