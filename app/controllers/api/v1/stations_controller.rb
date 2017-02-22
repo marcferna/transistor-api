a9699f970ec0315789d098bdeb4eadfa14da31c3
Marc Fernandez <marc.fernandezg@gmail.com>
2017-02-21 22:39:43 -0800

    Added spotlight stations.

--- app/controllers/api/v1/stations_controller.rb
+++ app/controllers/api/v1/stations_controller.rb
@@ -7,6 +7,7 @@ module Api::V1
       stations = Rails.cache.fetch 'stations/top500', expires_in: 1.day do
         client.top_500
       end
+      stations += spotlight_stations
       render json: stations
     end
 
@@ -26,6 +27,25 @@ module Api::V1
 
     def client
       @client ||= Shoutcast::Api::Client.new(key: ENV['SHOUTCAST_API_KEY'])
+    end
+
+    def spotlight_stations
+      stations_maximafm = Shoutcast::Api::Station.new
+      stations_maximafm.id = '518832'
+      stations_maximafm.name = 'Maxima FM'
+      stations_maximafm.genres = ["House"]
+      stations_maximafm.logo = 'http://i.radionomy.com/documents/radio/'\
+        'fdc1ceaa-04a4-4604-be82-2606260b966d.s165.jpg'
+
+      stations_los40 = Shoutcast::Api::Station.new
+      stations_los40.id = '916267'
+      stations_los40.name = 'Los 40 Principales'
+      stations_los40.genres = ['Top 40']
+
+      [
+        stations_maximafm,
+        stations_los40
+      ]
     end
   end
 end
--- app/serializers/shoutcast/api/station_serializer.rb
+++ app/serializers/shoutcast/api/station_serializer.rb
@@ -1,6 +1,7 @@
 module Shoutcast::Api
   class StationSerializer < ActiveModel::Serializer
-    attributes :id, :name, :genre, :logo, :url, :discoverable
+    attributes :id, :name, :genre, :logo, :url, :discoverable, :spotlight,
+      :banner
 
     def genre
       object.genres.first
@@ -18,6 +19,24 @@ module Shoutcast::Api
         'Easy Listening',
         'Dance'
       ].include?(genre)
+    end
+
+    def spotlight
+      [
+        '518832',
+        '916267'
+      ].include?(object.id)
+    end
+
+    def banner
+      case object.id
+      when '518832'
+        'http://res.cloudinary.com/hyjp7k1ul/image/upload/v1487657315/'\
+        'spotlight/maxima-fm_u2jnwf.jpg'
+      when '916267'
+        'http://res.cloudinary.com/hyjp7k1ul/image/upload/v1487659856/'\
+        'spotlight/los-40_guc58l.jpg'
+      end
     end
   end
 end
